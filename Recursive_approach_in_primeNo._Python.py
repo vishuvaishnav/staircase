0440c993191e1dd538b4cf40dd2829308b407ea8
Vishu Vaishnav <76559999+VISHUVAISHNAV@users.noreply.github.com>
2022-09-27 12:51:22 +0530

    Update Recursive_approach_in_primeNo._Python.py

--- Recursive_approach_in_primeNo._Python.py
+++ Recursive_approach_in_primeNo._Python.py
@@ -1,10 +1,16 @@
-def prime(n):
-    if p%n==0:
-        return n
+def PrimeNumber(n):
+    def prime(n):
+        if p%n==0:
+            return n
+        else:
+            return prime(n-1)
+    if prime(p-1)!=1:
+        return False
     else:
-        return prime(n-1)
+        return True
 p=int(input("Enter number : "))
-if prime(p-1)!=1:
-    print("Not prime")
-else:
+if PrimeNumber(p):
     print("Prime")
+else:
+    print("Not Prime")
+
