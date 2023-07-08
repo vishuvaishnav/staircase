fcd1cc54199af806d86db823173c96851163838b
Vishu Vaishnav <76559999+VISHUVAISHNAV@users.noreply.github.com>
2023-07-08 12:14:19 +0530

    Update Optimized_bubble_sort.py

--- bubble_sort.py
+++ bubble_sort.py
@@ -1,9 +1,16 @@
+#optimized bubble sort
+
 arr = list(map(int,input("Enter : ").split()))
 
 for i in range(len(arr),-1,-1):
     #print(arr)
+    noSwaps = True
     for j in range(0,i-1,1):
+        #print(arr,arr[j],arr[j+1])
         if arr[j]>arr[j+1]:
             arr[j],arr[j+1]=arr[j+1],arr[j]
+            noSwaps = False
+    if(noSwaps):
+        break
             
 print(arr)
