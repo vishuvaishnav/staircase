769c76152a50611706119a9c9de4da289d170282
Vishu Vaishnav <76559999+VISHUVAISHNAV@users.noreply.github.com>
2023-07-09 21:49:23 +0530

    Update selectionSort.py

--- selectionSort.py
+++ selectionSort.py
@@ -4,7 +4,9 @@ def selectionSort(arr):
         for j in range(i+1,len(arr)):
             if arr[j]<arr[lowest]:
                 lowest=j
-        arr[i],arr[lowest]=arr[lowest],arr[i]
+        if lowest != i:
+            #print(i,lowest)
+            arr[i],arr[lowest]=arr[lowest],arr[i]
     return arr
 
 arr = list(map(int,input("Enter : ").split()))
